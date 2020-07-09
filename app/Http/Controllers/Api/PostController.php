c0eff5c4ef223b84bcd302f9435a4968930f863a
AhmedAdly20 <ahmedadly132@gmail.com>
2020-07-09 22:27:13 +0200

    Finish The Get Request Data of API

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\PostsResource;
+use App\Http\Resources\CommentsResource;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::paginate( env('POSTS_PER_PAGE') );
+        return new PostsResource( $posts );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find( $id );
+        return new PostResource( $post );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function comments( $id ){
+        $post = Post::find( $id );
+        $comments = $post->comments()->paginate( env( 'COMMENTS_PER_PAGE' ) );
+        return new CommentsResource( $comments );
+    }
+}
