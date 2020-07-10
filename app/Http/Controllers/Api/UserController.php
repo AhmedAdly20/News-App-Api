20e4061d665d191f7194696a31ecb5d20e7040d9
AhmedAdly20 <ahmedadly132@gmail.com>
2020-07-10 13:06:28 +0200

    Update User

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -67,7 +67,15 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+        if( $request->has('name') ){
+            $user->name = $request->get('name');
+        }
+        if( $request->has('avatar') ){
+            $user->avatar = $request->get('avatar');
+        }
+        $user->save();
+        return new UserResource( $user );
     }
 
     /**
